e96da8b2625f808faef4605c4934169395141592
christian gonzalez <chgl10@gmail.com>
2024-07-15 20:01:51 -0500

    Se abordaron fundamentos de Python

---
+++ fundamentos.py
@@ -0,0 +1,12 @@
+name="Christian"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+last_name="Gonzalez Lopez"
+print(name*5)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name " " last_name)
