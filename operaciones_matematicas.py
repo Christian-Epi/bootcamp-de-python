2aa6ac372d1d88be52e2d13722092688fd694ebe
Christian-Epi <chgl10@gmail.com>
2024-07-17 20:48:55 -0500

    Operaciones matematicas

---
+++ operaciones_matematicas.py
@@ -0,0 +1,37 @@
+a=10
+b=3
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+# En Python no hay división entera
+# // para obtener la división entera
+# ** para obtener la potencia
+
+print(a//b)
+print(a**b)
+
+#Artificios
+a*=2
+print(a)
+a*=2
+print(a)
+
+operacion_1 = 2+3*4
+operacion_2 = 2+(3*4)
+operacion_3 = (2+3)*4
+operacion_4 = (2+3)*(4**2)/8-1
+print (operacion_1)
+print (operacion_2)
+print (operacion_3)
+print (operacion_4)
+
