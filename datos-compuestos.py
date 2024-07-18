948962b00cd77b5dd77da918444af2da2a180726
Christian-Epi <chgl10@gmail.com>
2024-07-17 19:42:03 -0500

    Abordamos datos compuestos

---
+++ datos-compuestos.py
@@ -0,0 +1,12 @@
+# LISTA
+conjunto={"nombre","Christian Gonzalez","Tecnotutoriales", True, 170, 170}
+print(conjunto)
+
+# Crear un Dicionario
+diccionario = {
+    "nombre": "Christian",
+    "apellido": "Gonzalez",
+    "estatura": 170,
+    "esta_feliz": True,
+}
+print(diccionario)
