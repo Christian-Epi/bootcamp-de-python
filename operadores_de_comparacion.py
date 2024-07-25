dd0f5085382626497d26b326fc6cc8bb47b441ab
Christian-Epi <chgl10@gmail.com>
2024-07-24 19:17:36 -0500

    Abordamos opereadores de comparacion

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,31 @@
+es_igual_que = 5 == 5
+es_distinto = 5 != 6
+es_mayor_que = 5 > 6
+es_mayor_o_igual = 5 >= 6
+es_menor_que = 5 < 6
+es_menor_o_igual = 5 <= 6
+print(es_menor_o_igual) 
+print(es_igual_que)
+print(es_distinto)
+print(es_mayor_que)
+print(es_mayor_o_igual)
+print(es_menor_que)
+
+#Calculos Combinados
+a=5
+b=10
+c=20
+comparacion1 = a+b==c
+print(comparacion1)
+comparacion2 = 2020<2024
+print(comparacion2)
+contrasena_almacenada1 = "contrasena"
+#Comparar Usuarios
+contrasena_ingresada1 = "contrasena"
+print(contrasena_almacenada1 == contrasena_ingresada1)
+contrasena_almacenada2 = "123456"
+contrasena_ingresada2  = '123456'
+print(contrasena_almacenada2 == contrasena_ingresada2)
+contrasena_almacenada3 = '''123456'''
+contrasena_ingresada3  = '123456'
+print(contrasena_almacenada3 == contrasena_ingresada3)
