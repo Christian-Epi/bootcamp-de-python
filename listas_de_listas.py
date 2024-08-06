b00510005c7a30516284537052c6c8d1f3bd55ab
Christian-Epi <chgl10@gmail.com>
2024-08-05 20:58:41 -0500

    abordamos listad de listas

---
+++ listas_de_listas.py
@@ -0,0 +1,11 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix)
+print(matrix[0])
+
+#Tuplas
+numeros = (1,2,3,4,5)
+print(numeros)
+print(type(numeros))
+print(numeros[0])
+
+#numeros[0] = 10 se comenta porque sale error porque no se puedee cambiar tuplas
