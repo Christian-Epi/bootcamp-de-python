e45126718a8512681d71d62712f52bbe4ff1cb20
Christian-Epi <chgl10@gmail.com>
2024-08-21 19:37:03 -0500

    Abordamos inputs

---
+++ input_numeros.py
@@ -0,0 +1,6 @@
+#Pedirle un numero al usuario
+numero = input("Dame un numero Campista: ")
+# Multiplicar el numero por 2
+# Convertier el entero a float
+resultado = float(numero) * 2
+print(f'el doble de {numero} es {resultado}')
